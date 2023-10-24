e8078bbe9b8ffefd2ff045d9bb222b58d1c4505c
b4rtaz <b4rtaz@gmail.com>
2023-10-24 22:26:46 +0200

    0.0.8.

--- demos/webpack-app/src/template-creator.ts
+++ demos/webpack-app/src/template-creator.ts
@@ -1,17 +1,17 @@
 import { Editor } from 'mini-canvas-editor';
-import { MceRect, MceTextbox } from 'mini-canvas-core';
+import { MceImageJSON, MceRect, MceTextbox } from 'mini-canvas-core';
 
 import 'mini-canvas-editor/css/editor.css';
 
 export class App {
-	public static async create(): Promise<App> {
+	public static async create(reset: boolean): Promise<App> {
 		const placeholder = document.getElementById('placeholder') as HTMLElement;
 		const preview = document.getElementById('preview') as HTMLImageElement;
 
-		const saved = localStorage['mceTemplateCreator'];
+		const saved = Storage.tryGet();
 		let editor: Editor;
-		if (saved) {
-			editor = await Editor.createFromJSON(JSON.parse(saved), placeholder, {});
+		if (saved && !reset) {
+			editor = await Editor.createFromJSON(saved, placeholder, {});
 		} else {
 			editor = Editor.createBlank(placeholder, 400, 300, {});
 			editor.add(
@@ -40,7 +40,8 @@ export class App {
 					fontSize: 40,
 					left: 140,
 					top: 52,
-					width: 240
+					width: 240,
+					maxHeight: 60
 				})
 			);
 			editor.add(
@@ -49,7 +50,8 @@ export class App {
 					left: 140,
 					top: 120,
 					width: 240,
-					fill: '#8c8c8c'
+					fill: '#8c8c8c',
+					maxHeight: 30
 				})
 			);
 		}
@@ -59,7 +61,7 @@ export class App {
 			preview.src = png;
 
 			const json = editor.toJSON();
-			localStorage['mceTemplateCreator'] = JSON.stringify(json);
+			Storage.set(json);
 		});
 		const app = new App(editor, preview);
 		editor.onChanged.subscribe(app.reloadPreview);
@@ -86,7 +88,7 @@ export class App {
 		layers.forEach(layer => {
 			if (layer.type === 'textbox') {
 				const text = replacer.getText(layer);
-				const newText = text.replace('$name', 'Tosiek').replace('$time', new Date().toLocaleTimeString());
+				const newText = text.replace('$name', 'Tosiek').replace('$time', new Date().toLocaleString());
 				if (newText !== text) {
 					replacer.replaceText(layer, newText);
 				}
@@ -98,6 +100,43 @@ export class App {
 
 		this.preview.src = canvas.exportToDataURL('png');
 	};
+
+	public async destroy() {
+		await this.editor.destroy();
+	}
 }
 
-document.addEventListener('DOMContentLoaded', App.create, false);
+const localStorageKey = 'mceTemplateCreator_v2';
+
+export class Storage {
+	public static tryGet(): MceImageJSON | undefined {
+		const raw = localStorage[localStorageKey];
+		return raw ? JSON.parse(raw) : undefined;
+	}
+
+	public static set(json: MceImageJSON) {
+		localStorage[localStorageKey] = JSON.stringify(json);
+	}
+
+	public static clear() {
+		localStorage.removeItem(localStorageKey);
+	}
+}
+
+async function load() {
+	const resetTemplateButton = document.getElementById('resetTemplateButton');
+	let app = await App.create(false);
+
+	resetTemplateButton?.addEventListener(
+		'click',
+		async e => {
+			e.preventDefault();
+			await app.destroy();
+			Storage.clear();
+			app = await App.create(true);
+		},
+		false
+	);
+}
+
+document.addEventListener('DOMContentLoaded', load, false);
